bbaa5567cd186a018dae73a2209a8a1021ca8348
chengying <hollye2558@gmail.com>
2023-02-03 14:19:08 -0500

    completed

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,3 +1,38 @@
+# class Person
+#   attr_accessor :partner, :name
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def get_married(person)
+#     if person.is_a?(Person)
+#     self.partner = person
+#     person.partner = self
+#     else
+#       begin
+#       raise PartnerError
+#       rescue PartnerError => PartnerError
+#         puts error.message
+#     end
+#   end
+# # end
+
+#   class PartnerError < StandardError
+#     def message
+#       "you must give the get_married method an argument of an instance of the Person class!"
+#     end
+#   end
+
+# end
+
+# beyonce = Person.new("Beyonce")
+# beyonce.get_married("Jay-Z")
+# puts beyonce.name
+
+
+
+
 class Person
   attr_accessor :partner, :name
 
@@ -6,16 +41,25 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    if person.is_a?(Person)
+      self.partner = person
+      person.partner = self
+    else
+      begin
+        raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+      end
+    end
   end
 
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
+  end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
